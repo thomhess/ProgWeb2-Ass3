f4a4ce47b882536faf19786cb2a187bb4ec838ee
Mats Gundersen <matsgundersen@hotmail.com>
2017-05-30 14:13:11 +0200

    css endringer

--- public/js/ajax-index.js
+++ public/js/ajax-index.js
@@ -21,7 +21,7 @@ function loadCat(id){
     }
     
     function writePosts (response) {
-        $('#posts').append('<div class="card col-xs-12" id="card'+ response.id +'"><img class="col-xs-12" src="../storage/app/'+response.img+'" alt="Card image cap"><h2><a href="posts/'+ response.id +'">'+ response.title +'</a></h2></div>');
+        $('#posts').append('<div class="card col-xs-12 col-md-4 " id="card'+ response.id +'"><img class="img-responsive" src="../storage/app/'+response.img+'" alt="Card image cap"><h2><a href="posts/'+ response.id +'">'+ response.title +'</a></h2></div>');
     }
     
     loadAll();
